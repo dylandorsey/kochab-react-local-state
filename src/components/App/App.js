93875b905e835b5438c81e90c0563ff64156432f
Dylan Dorsey <dylan.dorsey@gmail.com>
2018-05-15 14:55:43 -0500

    user and place in a user propertya

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,9 +7,11 @@ class App extends Component {
     super(props);
 
     this.state = {
-      user: 'Dash Rendar',
-      place: '' 
-    }
+      user: {
+        name: 'Dash Rendar',
+        place: 'Corellia',
+      }
+    };
 
     // make 'this' in handleChange the same as 'this' in the constructor
     // similar to const self = this;
@@ -25,33 +27,46 @@ class App extends Component {
     // this.state.user = event.target.value;
     // ^^ nice try. React hates this and will not do what you want if you try it like this.
     // instead:
-    this.setState({user: event.target.value});
+    this.setState({
+      user: {
+        name: event.target.value,
+        place: this.state.user.place,
+      }
+    });
   }
+
   handlePlaceChange = (event) => {
     // event is sometimes called evt, ev, e. it could be taco, right)
     console.log('event.target.value', event.target.value);
-    this.setState({place: event.target.value});
+    this.setState({
+      user: {
+        name: this.state.user.name,
+        place: event.target.value,
+      }
+    });
   }
-  handleClick = (event) => {
-    console.log(this.state);
+    handleClick = (event) => {
+      console.log(this.state.user);
+    };
+
+
+    render() {
+      return (
+        <div className="App">
+          The current user is {this.state.user.name}.
+        <br></br>
+          The current object is {JSON.stringify(this.state)}.
+        <br></br>
+          User: <input onChange={this.handleUserChange} placeholder="User Name" />
+          <br></br>
+          Place: <input onChange={this.handlePlaceChange} placeholder="Place Name" />
+          <br></br>
+          {this.state.user.name} is from {this.state.user.place}
+          <br></br>
+          Submit to console: <button onClick={this.handleClick}>Put the clicker here</button>
+        </div>
+      );
+    }
   }
-  render() {
-    return (
-      <div className="App">
-        The current user is {this.state.user}.
-        <br></br>
-        The current object is {JSON.stringify(this.state)}.
-        <br></br>
-        User: <input onChange={this.handleUserChange} placeholder="User Name"/>
-        <br></br>
-        Place: <input onChange={this.handlePlaceChange} placeholder="Place Name"/>
-        <br></br>
-        {this.state.user} is from {this.state.place}
-        <br></br>
-        Submit to console: <button onClick={this.handleClick}>Put the clicker here</button>
-      </div>
-    );
-  }
-}
 
 export default App;
